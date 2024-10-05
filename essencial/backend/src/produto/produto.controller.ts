376e5b62d54da809dc046f3ab146217d939c9030
Danilo <danilogmoura@gmail.com>
2024-10-05 00:21:41 -0300

    Adicionado recurso buscar produto por id

--- essencial/backend/src/produto/produto.controller.ts
+++ essencial/backend/src/produto/produto.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { Produto, produtos } from 'src/core'
 
 @Controller('produtos')
 export class ProdutoController {
 
     @Get()
-    listar(): Produto[] {
+    async listar(): Promise<Produto[]> {
         return produtos.map((produto) => ({
             ...produto,
             especificacoes: { destaque: produto.especificacoes.destaque },
         }));
+    }
+
+    @Get(":id")
+    async buscar(@Param("id") id: string): Promise<Produto | null> {
+        const produto = produtos.find(produto => produto.id === +id)
+        return produto ?? null
     }
 }
