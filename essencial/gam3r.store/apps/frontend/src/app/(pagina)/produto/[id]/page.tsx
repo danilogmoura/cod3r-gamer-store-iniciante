2367d39ea8de7fadd7382448d5d4b168df35ad2b
Danilo <danilogmoura@gmail.com>
2024-10-05 22:02:33 -0300

    Informações do Produto

--- essencial/gam3r.store/apps/frontend/src/app/(pagina)/produto/[id]/page.tsx
+++ essencial/gam3r.store/apps/frontend/src/app/(pagina)/produto/[id]/page.tsx
@@ -4,10 +4,6 @@ import useProdutos from '@/data/hooks/useProdutos'
 import { useEffect, useState } from 'react'
 import TituloProduto from '@/components/produto/TituloProduto'
 import InformacoesProduto from '@/components/produto/InformacoesProduto'
-import BannerCompra from '@/components/produto/BannerCompra'
-import MedidorDePreco from '@/components/produto/MedidorDePreco'
-import AvaliacoesUsuarios from '@/components/produto/AvaliacoesUsuarios'
-import AvaliacaoEspecializada from '@/components/produto/AvaliacaoEspecializada'
 import ProdutoNaoEncontrado from '@/components/produto/ProdutoNaoEncontrado'
 
 export default function PaginaProduto(props: any) {
@@ -23,13 +19,7 @@ export default function PaginaProduto(props: any) {
             <div className="flex flex-col gap-10">
                 <TituloProduto produto={produto} />
                 <InformacoesProduto produto={produto} />
-                <BannerCompra produto={produto} />
-                <MedidorDePreco produto={produto} />
             </div>
-            <AvaliacoesUsuarios produto={produto} />
-            <AvaliacaoEspecializada produto={produto} />
         </div>
-    ) : (
-        <ProdutoNaoEncontrado />
-    )
+    ) : (<ProdutoNaoEncontrado />)
 }
